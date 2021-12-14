45b5b1e09d2c7f135186da60aaef79e6747b504f
SQSJWONG <SQSJWONG@N5592.slake.sssb.com>
2021-12-14 16:08:44 +0800

    first commit

---
+++ demo/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+// POJO - Plan Old Java Object ie.plain java class
+public class Greeting {
+	private final long id;
+
+	 private final String content;
+
+	 public Greeting(long id, String content) {
+	this.id = id;
+	this.content = content;
+	}
+
+	 public long getId() {
+	return id;
+	}
+
+	 public String getContent() {
+	return content;
+	}
+}
